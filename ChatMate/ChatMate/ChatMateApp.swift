2229e07ca8d25dcfeff9678bbee5a3158085f139
melis <melis.sakic.12@gmail.com>
2023-04-13 19:00:33 +0200

    - ChatMate initial

---
+++ ChatMate/ChatMate/ChatMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatMateApp.swift
+//  ChatMate
+//
+//  Created by Serapion on 13. 4. 2023..
+//
+
+import SwiftUI
+
+@main
+struct ChatMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
